5c0bd13376cabd2d22a4e7a39334d052682cc669
Lakshit <Lakshitpaliwal9@gmail.com>
2024-10-09 10:49:10 +0530

    mongo1

--- MOngo/Assingment1/db/index.js
+++ MOngo/Assingment1/db/index.js
@@ -1,19 +1,30 @@
 const mongoose = require('mongoose');
+const { string } = require('zod');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb://localhost:27017/pactice');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  username:String,
+  password: String, 
+
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username:String,
+    password: String,
+    purchasedCourses:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'course',
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title:String,
+    description:String,
+    imagelink:String,
+    price:Number,
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);

7ae4a8ed271dbb5cdcb20e2e41e9687c70301fdb
Lakshit <Lakshitpaliwal9@gmail.com>
2024-10-17 09:29:03 +0530

    map

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import{useState} from 'react';
 function App() {
-  const [count, setCount] = useState(0)
-
+ const[count,setCount]=useState(0);
+ function onClickHandler(){
+  setCount(count +1);
+ }
   return (
     <div>
-    hi
+      <button onClick={onClickHandler}>
+     Count{count}
+      </button>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
